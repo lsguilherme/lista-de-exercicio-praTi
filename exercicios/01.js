c8fb42c6bf46844dc776d86343985afc11d88566
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-05-07 21:21:36 -0300

    Create: verificar se um número é par ou ímpar.

---
+++ exercicios/01.js
@@ -0,0 +1,23 @@
+/* 
+    1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+
+    Entrada: um número inteiro - Integer.
+    Lógica: verifica se é par ou ímpar - if.
+    Saída:  retorna se é par ou ímpar.
+*/
+var prompt = require("prompt-sync")();
+
+function parOuImpar(numero) {
+  if (isNaN(numero)) {
+    return `Erro: Por favor digite um número válido.`;
+  }
+
+  if (numero % 2 === 0) {
+    return `${numero} é par.`;
+  }
+  return `${numero} é impar.`;
+}
+
+let teste = Number(prompt("Digite um número: "));
+
+console.log(parOuImpar(teste));
